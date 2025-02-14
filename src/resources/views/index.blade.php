a9467543527d6d234b831652a333f69bf4aaff5c
yuriko <futabagusa.yuriko@gmail.com>
2025-02-14 14:45:05 +0900

    商品一覧画面のビュー作成、スタイル実装

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,20 +6,39 @@
 
 @section('content')
 <div>
-<h1>商品一覧画面だよ</h1>
-<!-- タブ -->
+    <!-- タブ -->
     <div class="tabs">
-        <a href="{{ route('index') }}" class="tab {{ request()->get('tab') != 'mylist' ? 'active' : '' }}">おすすめ</a>
-        <a href="{{ route('index', ['tab' => 'mylist']) }}" class="tab {{ request()->get('tab') == 'mylist' ? 'active' : '' }}">マイリスト</a>
+        <a href="{{ route('index') }}" class="tabs__link {{ request()->get('tab') != 'mylist' ? 'tabs__link--active' : '' }}">おすすめ</a>
+        <a href="{{ route('index', ['tab' => 'mylist']) }}" class="tabs__link {{ request()->get('tab') == 'mylist' ? 'tabs__link--active' : '' }}">マイリスト</a>
     </div>
 
     <!-- 商品一覧表示 -->
     <div class="product-list">
         @foreach ($products as $product)
-        <div>
-            <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}">
-            <p>{{ $product->name }}</p>
-        </div>
+            @if (auth()->check() && auth()->user()->id !== $product->user_id) <!-- 自分が出品した商品は非表示 -->
+                <div class="product-list__item">
+                    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}">
+                    <p class="product-list__name">{{ $product->name }}</p>
+                
+                    @php
+                        $sold = \App\Models\Order::where('product_id', $product->id)->exists();//商品が注文されているか
+                    @endphp
+                    @if ($sold)
+                        <span class="product-list__sold">Sold</span>
+                    @endif
+                </div>
+            @elseif (!auth()->check())
+                <div class="product-list__item">
+                    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}">
+                    <p class="product-list__name">{{ $product->name }}</p>
+                    @php
+                        $sold = \App\Models\Order::where('product_id', $product->id)->exists();//商品が注文されているか
+                    @endphp
+                    @if ($sold)
+                        <span class="product-list__sold">Sold</span>
+                    @endif
+                </div>
+            @endif
         @endforeach
     </div>
 </div>
